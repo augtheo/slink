89eadc1e83cf0cc4bde7d488116503101d926924
Augustine Theodore <16444359+augtheo@users.noreply.github.com>
2024-04-29 00:41:02 +0530

    feat: Use vite and shadcn/ui for slink frontend Nix this project Remove protoc references Refactor files

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        name = "slink";
+
+        buildInputs = with pkgs;[
+          nodejs
+          # You can set the major version of Node.js to a specific one instead
+          # of the default version
+          # nodejs-19_x
+
+          # You can choose pnpm, yarn, or none (npm).
+          # nodePackages.pnpm
+
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+
+          prettierd
+
+          go
+          gopls
+        ];
+      };
+    }
+  );
+}
