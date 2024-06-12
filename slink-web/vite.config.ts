cd06466636cbed3e69440b03b9acd797307379db
Augustine Theodore <16444359+augtheo@users.noreply.github.com>
2024-06-12 12:45:29 +0530

    fix: change base path

--- slink-web/vite.config.ts
+++ slink-web/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
+  base: "/app",
   plugins: [react(), viteCompression()],
   server: {
     watch: {
